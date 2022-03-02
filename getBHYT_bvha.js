a514f331d26f34ccf08d11acd38e509c89494d26
Truong Le Khac <72380349+truongkt2002@users.noreply.github.com>
2022-03-02 21:00:56 +0700

    Update getBHYT_bvha.js

--- getBHYT_bvha.js
+++ getBHYT_bvha.js
@@ -19,9 +19,9 @@ function LayThongBHYT_LichSuKham() {
 			function(t)
 			{
 				//tendangnhap=$(t).find("#motathamso123").val();
-				var matkhau1=$(t).find("#motathamso124").val();
+				var chuoi123=$(t).find("#motathamso124").val();
 				matkhau1 = MD5(matkhau1);
-				console.log('ddđ '+matkhau);
+				console.log('ddđ '+chuoi123);
 				//console.log($('matkhau1'));
 		
 				tendangnhap = '31020_BV';
